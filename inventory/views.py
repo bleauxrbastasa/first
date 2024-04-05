72c4dba1da6678ef5f326d7cc8621eecb04223d2
bleauxrbastasa <bleauxrbastasa@gmail.com>
2024-04-05 11:32:15 +0800

    TRACY

--- inventory/views.py
+++ inventory/views.py
@@ -1,49 +1,17 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import *
-from .forms import itemForm
+from .models import Food
+from .forms import FoodForm
 
-# Create your views here.
+def add_food(request):
+    if request.method == 'POST':
+        form = FoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = FoodForm()
+    return render(request, 'inventory/add_product.html', {'form': form})
 
-def home(request):
-	return render(request, 'inventory/dashboard.html')
-
-def inventory(request):
-	item = inventoryItem.objects.all()
-	return render(request, 'inventory/inventory.html', {'item': item})
-
-def customer(request):
-	return render(request, 'inventory/customer.html')
-
-def createItem(request):
-	form = itemForm()
-	if request.method == 'POST':
-		form = itemForm(request.POST, request.FILES)
-		if form.is_valid():
-			form.save()
-			return redirect('/inventory')
-
-	context = {'form': form}
-	return render(request, 'inventory/item_form.html', context)
-
-def updateItem(request, pk):
-	item = inventoryItem.objects.get(id=pk)
-	form = itemForm(instance=item)
-
-	if request.method == 'POST':
-		form = itemForm(request.POST, request.FILES, instance=item)
-		if form.is_valid():
-			form.save()
-			return redirect('/inventory')
-
-	context = {'form': form}
-	return render(request, 'inventory/item_form.html', context)
-
-def deleteItem(request, pk):
-	item = inventoryItem.objects.get(id=pk)
-
-	if request.method == "POST":
-		item.delete()
-		return redirect('inventory')
-	context = {'item': item}
-	return render(request, 'inventory/delete.html', context)
+def food_list(request):
+    foods = Food.objects.all()
+    return render(request, 'inventory/product_list.html', {'foods': foods})
